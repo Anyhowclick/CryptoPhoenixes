8511fc712bcbe4b93849db43b407bbd3b83ed4aa
Anyhowclick <howeilun@hotmail.com>
2018-03-14 12:14:53 +0800

    Changed header font size for smaller devices

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { Jumbotron, Container, Col, Row, Button,
   Card, CardImg, CardBody, CardTitle, CardSubtitle, Progress } from 'reactstrap'
 import {MAX_COOLDOWN, MAX_POWER, MAX_DIVIDENDS} from './Constants.js'
-import Odometer from 'react-odometerjs'
-
 
 export default class Home extends Component {
   render() {
